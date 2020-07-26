2cf7676d5977c88b64438e20dfcacbeb2b9fa06b
MY-PC\MY PC <zubir211@gmail.com>
2020-07-26 21:13:43 +0800

    First upload

---
+++ inv_project p3/public_html/database/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","project_inv");
+
+
+ ?>
---
+++ inv_project p3/public_html/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Inventory Management System </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="./js/main.js"></script>
+  </head>
+  <body>
+    <!-- Navabr -->
+<?php include_once("./templates/header.php"); ?>
+  <br><br>
+    <div class="container">
+        <div class="card mx-auto" style="width: 18rem;">
+          <img src="./images/login.png" style="width:60%;" class="card-img-top mx-auto" alt="Login Icon">
+          <div class="card-body">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary"><i class="fa fa-lock">&nbsp;</i>Login</button>
+            <span> <a href="#"> Register </a> </span>
+          </form>
+
+          </div>
+          <div class="card-footer"> <a href="#"> Forget Password </a> </div>
+        </div>
+
+    </div>
+
+
+
+  </body>
+</html>
